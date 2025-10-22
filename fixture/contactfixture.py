0b876640be14520a1b28a8e6c58b1658852025f1
Anastasia <apolyakova09@gmail.com>
2025-10-22 19:15:16 +0300

    выделены вспомогательный класс для контактов

--- fixture/contactfixture.py
+++ fixture/contactfixture.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from fixture.contact import ContactHelper
 
 class Contactfixture:
+
     def __init__(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(60)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -20,37 +23,6 @@ class Contactfixture:
          wd.find_element(By.NAME, "pass").send_keys(password)
          wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
-    def open_contact_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "add new").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        wd.find_element(By.NAME, "firstname").click()
-        wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
-        wd.find_element(By.NAME, "middlename").click()
-        wd.find_element(By.NAME, "middlename").clear()
-        wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
-        wd.find_element(By.NAME, "lastname").click()
-        wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
-        wd.find_element(By.NAME, "nickname").click()
-        wd.find_element(By.NAME, "nickname").clear()
-        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
-        wd.find_element(By.NAME, "address").click()
-        wd.find_element(By.NAME, "address").clear()
-        wd.find_element(By.NAME, "address").send_keys(contact.address)
-        wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "home").click()
-        wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "mobile").clear()
-        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[20]").click()
-
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "home page").click()
 
     def logout(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
 from fixture.contactfixture import Contactfixture
-import unittest
 from model.contact import Contact
 
 
@@ -16,11 +15,8 @@ def con(request):
 def test_test_add_contact(con):
     con.open_home_page()
     con.login(username="admin", password="secret")
-    con.open_contact_page()
-    con.create_contact(Contact( firstname="grsgsg", middlename="sdfgsfg", lastname="hjkt", nickname="hgf", address="fjvf45dfknj34", mobile="89232342342"))
-    con.return_to_home_page()
+    con.contact.open_contact_page()
+    con.contact.create(Contact(firstname="grsgsg", middlename="sdfgsfg", lastname="hjkt", nickname="hgf", address="fjvf45dfknj34", mobile="89232342342"))
+    con.contact.return_to_home_page()
     con.logout()
 
-
-if __name__ == "__main__":
-    unittest.main()
