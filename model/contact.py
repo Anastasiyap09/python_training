980f8566ef962b70d5133444414746831df761d8
Anastasia <apolyakova09@gmail.com>
2025-11-14 20:00:50 +0300

    доработала get_contact_list

--- model/contact.py
+++ model/contact.py
@@ -13,7 +13,7 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s (%s)" % (self.firstname, self.lastname, self.id)
+        return "%s:%s %s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
         return self.firstname  == other.firstname and self.lastname  == other.lastname  and (self.id is None or other.id == other.id)
